fa783eee5e0a64ec2e60e120e33652790e0ec639
Eredost <michael.coutin62@gmail.com>
2021-07-24 13:59:15 +0200

    feat: add Sport entity and repository classes

---
+++ src/Entity/Sport.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SportRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SportRepository::class)
+ */
+class Sport
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=60)
+     */
+    private string $label;
+}
---
+++ src/Repository/SportRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Sport;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class SportRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Sport::class);
+    }
+}
