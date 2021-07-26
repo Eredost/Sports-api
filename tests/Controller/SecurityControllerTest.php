d1769a02b14e4499363e75c8103f29b3636c4a27
Eredost <michael.coutin62@gmail.com>
2021-07-26 12:58:55 +0200

    test: add functional tests for SecurityController

---
+++ tests/Controller/SecurityControllerTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\DataFixtures\Providers\UserProvider;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SecurityControllerTest extends WebTestCase
+{
+    private ?KernelBrowser $client;
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+    }
+
+    private function loginRequest(string $body): void
+    {
+        $this->client->request(
+            'POST',
+            '/api/login',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            $body
+        );
+    }
+
+    public function testSuccessfulLogIn(): void
+    {
+        $userCredentials = UserProvider::getUsers()[0];
+        $this->loginRequest(json_encode([
+                'username' => $userCredentials['username'],
+                'password' => $userCredentials['plainPassword'],
+            ])
+        );
+        $data = json_decode($this->client->getResponse()->getContent(), true);
+
+        self::assertArrayHasKey('token', $data);
+        self::assertTrue($this->client->getResponse()->isOk());
+    }
+
+    public function testLogInWithMissingUsername(): void
+    {
+        $this->loginRequest(json_encode([
+                'password' => 'Pas3word#',
+            ])
+        );
+
+        self::assertTrue($this->client->getResponse()->isClientError());
+    }
+
+    public function testLogInWithMissingPassword(): void
+    {
+        $this->loginRequest(json_encode([
+                'username' => 'PolarBear',
+            ])
+        );
+
+        self::assertTrue($this->client->getResponse()->isClientError());
+    }
+
+    public function testLogInWithInvalidCredentials(): void
+    {
+        $userCredentials = UserProvider::getUsers()[0];
+        $this->loginRequest(json_encode([
+                'username' => $userCredentials['username'],
+                'password' => 'invalidPassword',
+            ])
+        );
+
+        self::assertTrue($this->client->getResponse()->isClientError());
+    }
+
+    public function testLogInWithMalformedJSONBody(): void
+    {
+        $this->loginRequest('["username" => "Servietsky", "plainPassword" => "Ylq?12$PeviTYdm8"]');
+
+        self::assertTrue($this->client->getResponse()->isClientError());
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->client = null;
+    }
+}
