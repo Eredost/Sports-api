8c1cfd4a06aa0ab710d145d5cef7dafde08bb0e8
Eredost <michael.coutin62@gmail.com>
2021-07-26 10:35:56 +0200

    test: add unit tests for validation constraints of sport entity

--- tests/Entity/SportTest.php
+++ tests/Entity/SportTest.php
@@ -2,7 +2,43 @@
 
 namespace App\Tests\Entity;
 
-class SportTest
-{
+use App\Entity\Sport;
+use App\Tests\Entity\Traits\AssertTrait;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
+class SportTest extends KernelTestCase
+{
+    use AssertTrait;
+
+    protected function getEntity(): Sport
+    {
+        return (new Sport())
+            ->setLabel('sportLabel')
+        ;
+    }
+
+    public function testValidEntity(): void
+    {
+        $this->assertHasErrors($this->getEntity());
+    }
+
+    public function testInvalidBlankLabel(): void
+    {
+        $this->assertHasErrors($this->getEntity()->setLabel(''), 1);
+    }
+
+    public function testInvalidLengthLabel(): void
+    {
+        $this->assertHasErrors($this->getEntity()->setLabel(str_repeat('*', 61)), 1);
+    }
+
+    public function testReturnTypeCreatedAt(): void
+    {
+        self::assertInstanceOf(\DateTimeInterface::class, $this->getEntity()->getCreatedAt());
+    }
+
+    public function testReturnTypeUpdatedAt(): void
+    {
+        self::assertNull($this->getEntity()->getUpdatedAt());
+    }
 }
