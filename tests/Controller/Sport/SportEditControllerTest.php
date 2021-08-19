f5a703b2b8f2db9570ffb10a31d4b504cde71aab
Eredost <michael.coutin62@gmail.com>
2021-08-19 15:35:13 +0200

    test: move sport edit, list and delete functional tests into their own controller

--- tests/Controller/Sport/SportEditControllerTest.php
+++ tests/Controller/Sport/SportEditControllerTest.php
@@ -2,7 +2,115 @@
 
 namespace App\Tests\Controller\Sport;
 
-class SportEditControllerTest
-{
+use App\Entity\Sport;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
+class SportEditControllerTest extends AbstractSportControllerTest
+{
+    protected function getRoute(): array
+    {
+        $sport = $this->entityManager
+            ->getRepository(Sport::class)
+            ->findOneBy([])
+        ;
+
+        return [
+            'method' => Request::METHOD_PUT,
+            'path'   => '/api/sports/' . $sport->getId(),
+        ];
+    }
+
+    public function testSportEdit(): void
+    {
+        $route = $this->getRoute();
+        $this->client->request(
+            $route['method'],
+            $route['path'],
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode([
+                'label' => 'newLabel',
+            ]),
+        );
+        $modifiedSport = json_decode($this->client->getResponse()->getContent(), true);
+
+        self::assertTrue($this->client->getResponse()->isOk());
+        self::assertEquals('newLabel', $modifiedSport['label']);
+    }
+
+    public function testSportEditWithNonExistentSport(): void
+    {
+        $route = $this->getRoute();
+        $this->client->request(
+            $route['method'],
+            '/api/sports/0',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode([
+                'label' => 'labelValue',
+            ]),
+        );
+
+        self::assertEquals(Response::HTTP_NOT_FOUND, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testSportEditWithInvalidValue(): void
+    {
+        $route= $this->getRoute();
+        $this->client->request(
+            $route['method'],
+            $route['path'],
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode([
+                'label' => '',
+            ]),
+        );
+
+        self::assertEquals(Response::HTTP_BAD_REQUEST, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testCreationWithMalformedJSONBody(): void
+    {
+        $route = $this->getRoute();
+        $this->client->request(
+            $route['method'],
+            $route['path'],
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            '{{}'
+        );
+
+        self::assertEquals(Response::HTTP_BAD_REQUEST, $this->client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * @dataProvider notAllowedMethodsProvider
+     */
+    public function testCreationWithNotAllowedHTTPMethod($method): void
+    {
+        $route = $this->getRoute();
+        $this->client->request(
+            $method,
+            $route['path'],
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json']
+        );
+
+        self::assertEquals(Response::HTTP_METHOD_NOT_ALLOWED, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function notAllowedMethodsProvider(): array
+    {
+        return [
+            [Request::METHOD_POST],
+            [Request::METHOD_PATCH],
+        ];
+    }
 }
--- tests/Controller/Sport/SportListControllerTest.php
+++ tests/Controller/Sport/SportListControllerTest.php
@@ -2,7 +2,66 @@
 
 namespace App\Tests\Controller\Sport;
 
-class SportListControllerTest
-{
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
+class SportListControllerTest extends AbstractSportControllerTest
+{
+    protected function getRoute(): array
+    {
+        return [
+            'method' => Request::METHOD_GET,
+            'path'   => '/api/sports',
+        ];
+    }
+
+    public function testSportList(): void
+    {
+        $route = $this->getRoute();
+        $this->client->request(
+            $route['method'],
+            $route['path'],
+            ['limit' => 3]
+        );
+        $sports = json_decode($this->client->getResponse()->getContent(), true);
+
+        self::assertTrue($this->client->getResponse()->isOk());
+        self::assertCount(3, $sports);
+    }
+
+    public function testSportListWithInvalidParams(): void
+    {
+        $route = $this->getRoute();
+        $this->client->request(
+            $route['method'],
+            $route['path'],
+            ['offset' => '','limit' => -3, 'order' => 'any']
+        );
+        $sports = json_decode($this->client->getResponse()->getContent(), true);
+
+        self::assertTrue($this->client->getResponse()->isOk());
+        self::assertCount(10, $sports);
+    }
+
+    /**
+     * @dataProvider notAllowedMethodsProvider
+     */
+    public function testCreationWithNotAllowedHTTPMethod($method): void
+    {
+        $route = $this->getRoute();
+        $this->client->request(
+            $method,
+            $route['path'],
+        );
+
+        self::assertEquals(Response::HTTP_METHOD_NOT_ALLOWED, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function notAllowedMethodsProvider(): array
+    {
+        return [
+            [Request::METHOD_DELETE],
+            [Request::METHOD_PUT],
+        ];
+    }
 }
